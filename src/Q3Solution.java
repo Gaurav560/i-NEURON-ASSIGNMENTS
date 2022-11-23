d8e39eacbf58c58ae62343a32089b196d819b86f
GAURAV SHARMA <gauravofficial056@gmail.com>
2022-11-23 16:52:39 +0530

    commit 1

--- src/Q3Solution.java
+++ src/Q3Solution.java
@@ -1,6 +1,6 @@
 public class Q3Solution {
     public static void main(String[] args) {
-        int n=14;
+        int n=14;ff
         for (int row = 0; row < n; row++) {
             for (int column = 0; column < n; column++) {
                 if (row+column<=(n-1)/2||row==0||column==0||column-row>=((n/2)-1)||row==n-1||column==n-1){
