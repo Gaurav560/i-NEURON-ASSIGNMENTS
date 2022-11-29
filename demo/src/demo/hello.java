5c51db1db162baefedf3cbec028d321637c401b7
Gaurav560 <gsharma1154@gmail.com>
2022-11-29 14:54:50 +0530

    hello

--- demo/src/demo/hello.java
+++ demo/src/demo/hello.java
@@ -3,8 +3,8 @@ package demo;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello java");
-
+		System.out.println(" java hello java");
+             
 	}
 
 }
